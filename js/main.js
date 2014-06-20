29cc58e37643958fa7b8ee5b929cf712c14034d5
Theodore Kluge <villa7.kluge5@gmail.com>
2014-06-19 21:32:06 -0400

    made it better :D

--- js/main.js
+++ js/main.js
@@ -7,13 +7,21 @@ $(
 	function () {
 	    $(document).keydown(
 			function (event) {
-			    keytoggle(event);
+                if (!col.active) {
+    			    keytoggle(event);
+                }
+                if (col.active) {
+                    col.entercol(event);
+                }
 			}
 		);
 	}
 );
 
 var disDbg = false;
+var checkRestart, content, prevcontent = null;
+var typspeed = 35;
+var canSetClr = false;
 
 var typ = {
     text: null,
@@ -23,22 +31,24 @@ var typ = {
     speed: 0.5, // speed
     file: "", //file to type
     init: function () {
+
         cTimer = setInterval(function () { 
             typ.updateLC(); 
         }, 500);
-
-        if (typ.file == 'texts/foxy.txt') {
-            var typspeed = 10;
-        } else {
-            var typspeed = 35;
-        }
 
         setTimeout(function() {
             clearInterval(cTimer);
             tID = setInterval(function() {
                 typ.addText(1);
-            }, typspeed);
-        }, 3000);
+            }, 35/*typspeed*/);
+        }, 3000); //blink the cursor for 3 sec
+
+        setTimeout(function() {
+            clearInterval(checkRestart);
+            checkRestart = setInterval(function() {
+                checkFinish();    
+            }, 7000); //check to see if content has changed
+        }, 30000); //wait 30sec before starting checks since most will be more than 30sec anyway
 
         $.get(typ.file, function (data) {
             typ.text = data;
@@ -91,7 +101,13 @@ var typ = {
         //     typ.stopTyper();
         // }
 
-        $('.debug').html('HTML: ' + typ.content().length + '<br>Char: ' + $('.console').text().length + '<br>Length: ' + typ.text.length);
+        $('.debug').html('HTML: ' + typ.content().length + 
+            '<br>Char: ' + $('.console').text().length + 
+            '<br>Length: ' + typ.text.length +
+            '<br>Using: /' + typ.file + 
+            '<br>Speed: ' + typspeed + 'ms delay ' + 
+            '<br>ColorT: ' + col.text + 
+            '<br>ColorB: ' + col.bg);
     },
 
     updateLC: function () { // blinking cursor
@@ -112,13 +128,163 @@ var typ = {
     }
 }
 
+var col = {
+    text: '#0F0',
+    bg: '#000',
+    active: false,
+    allowC1: false,
+    allowC2: false,
+
+    openPicker: function() {
+        $('.colorpicker').toggle(500);
+        this.allowC1 = true;
+        this.active = true;
+        setTimeout(function() {
+        canSetClr = true;
+        }, 100);
+    },
+
+    setColor: function(hex) {
+
+        if (canSetClr) {
+            if (this.allowC1) {
+                $('#clr-b').css('background', hex);
+                this.bg = hex;
+                this.allowC2 = true;
+                this.allowC1 = false;
+            } else if (this.allowC2) {
+                $('#clr-t').css('background', hex);
+                this.text = hex;
+                this.allowC2 = false;
+                this.active = false;
+                canSetClr = false;
+                $('.colorpicker').toggle(500);
+                this.set(this.bg, this.text);
+            }
+        }
+    },
+
+    set: function(bg, text) {
+        $('body').css('background', bg);
+        $('.console').css('color', text);
+    },
+
+    entercol: function(id) {
+        if (id.keyCode == 48) {this.setColor('#000');} //black
+        if (id.keyCode == 49) {this.setColor('#008'); } //blue
+        if (id.keyCode == 50) {this.setColor('#080'); } //green
+        if (id.keyCode == 51) {this.setColor('#00ffff'); } //aqua
+        if (id.keyCode == 52) {this.setColor('#F00'); } //red
+        if (id.keyCode == 53) {this.setColor('#85144B'); } //purple
+        if (id.keyCode == 54) {this.setColor('#FF851B'); } //gold
+        if (id.keyCode == 55) {this.setColor('#AAA'); } //lightgray
+        if (id.keyCode == 56) {this.setColor('#888'); } //darkgray 
+        if (id.keyCode == 57) {this.setColor('#0074D9'); } //lightblue 
+        if (id.keyCode == 65) {this.setColor('#0F0'); } //lightgreen 
+        if (id.keyCode == 66) {this.setColor('#7FDBFF'); } //lightaqua 
+        if (id.keyCode == 67) {this.setColor('#FF4136'); } //lightred 
+        if (id.keyCode == 68) {this.setColor('#F012BE'); } //lightpurple 
+        if (id.keyCode == 69) {this.setColor('#FFDC00'); } //lightyellow 
+        if (id.keyCode == 70) {this.setColor('#FFF'); } //white
+    },
+
+    entercolpre: function(b, t) {
+        if (b == 0) {this.setBackground('#000');} //black
+        if (b == 1) {this.setBackground('#008'); } //blue
+        if (b == 2) {this.setBackground('#080'); } //green
+        if (b == 3) {this.setBackground('#00ffff'); } //aqua
+        if (b == 4) {this.setBackground('#F00'); } //red
+        if (b == 5) {this.setBackground('#85144B'); } //purple
+        if (b == 6) {this.setBackground('#FF851B'); } //gold
+        if (b == 7) {this.setBackground('#AAA'); } //lightgray
+        if (b == 8) {this.setBackground('#888'); } //darkgray 
+        if (b == 9) {this.setBackground('#0074D9'); } //lightblue 
+        if (b == 'a') {this.setBackground('#0F0'); } //lightgreen 
+        if (b == 'b') {this.setBackground('#7FDBFF'); } //lightaqua 
+        if (b == 'c') {this.setBackground('#FF4136'); } //lightred 
+        if (b == 'd') {this.setBackground('#F012BE'); } //lightpurple 
+        if (b == 'e') {this.setBackground('#FFDC00'); } //lightyellow 
+        if (b == 'f') {this.setBackground('#FFF'); } //white
+
+        /*switch (b) { //alternate way
+            case 0: this.setBackground('#111'); break;
+        }*/
+
+        if (t == 0) {this.setTextC('#000');} //black
+        if (t == 1) {this.setTextC('#008'); } //blue
+        if (t == 2) {this.setTextC('#080'); } //green
+        if (t == 3) {this.setTextC('#00ffff'); } //aqua
+        if (t == 4) {this.setTextC('#F00'); } //red
+        if (t == 5) {this.setTextC('#85144B'); } //purple
+        if (t == 6) {this.setTextC('#FF851B'); } //gold
+        if (t == 7) {this.setTextC('#AAA'); } //lightgray
+        if (t == 8) {this.setTextC('#888'); } //darkgray 
+        if (t == 9) {this.setTextC('#0074D9'); } //lightblue 
+        if (t == 'a') {this.setTextC('#0F0'); } //lightgreen 
+        if (t == 'b') {this.setTextC('#7FDBFF'); } //lightaqua 
+        if (t == 'c') {this.setTextC('#FF4136'); } //lightred 
+        if (t == 'd') {this.setTextC('#F012BE'); } //lightpurple 
+        if (t == 'e') {this.setTextC('#FFDC00'); } //lightyellow 
+        if (t == 'f') {this.setTextC('#FFF'); } //white
+    },
+    setBackground: function(hex) {
+        console.log('setbg ' + hex);
+        this.bg = hex;
+        $('body').css('background', hex);
+    },
+    setTextC: function(hex) {
+        console.log('settextc ' + hex);
+        this.text = hex;
+        $('.console').css('color', hex);
+    }
+}
+
 keytoggle = function (id) {
-    if (id == 68) {
-        if (!disDbg) {
-            $('.debug').css('display','block');
-        } else {
-            $('.debug').css('display','none');
+    if (id.keyCode == 68) {
+        $('.debug').toggle(500);
+    }
+    if (id.keyCode == 82) {
+        $('.readme').toggle(500);
+    }
+    if (id.keyCode == 67) {
+        if (!col.active) {
+            col.openPicker();
+            
         }
     }
 }
 
+checkFinish = function() {
+
+    prevcontent = content;
+    content = typ.content();
+
+    if (content == prevcontent) {
+
+        $('.console').empty();
+
+        typ.text = null;
+        typ.cTimer = null;
+        typ.tID = null;
+        typ.index = 0;
+        typ.file = '';
+
+        var rand = Math.floor(Math.random() * fileToUse.length);
+
+        typ.file = fileToUse[rand];
+        console.log('Using random: ' + rand);
+        console.log('Using file "' + typ.file + '"');
+
+        typ.init();
+    }
+
+}
+
+getParamByName = function(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+console.log(getParamByName('b'), getParamByName('t'));
